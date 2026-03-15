daff33d8d19da98f30f07180d8c56899e2265b02
Anna Vesenko <vesenko.anna@gmail.com>
2026-03-15 14:17:42 +0300

    Add files via upload

---
+++ Задача 1/solution_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+};
---
+++ Задача 1/solution_2.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+public:
+    int missingNumber(std::vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+};
